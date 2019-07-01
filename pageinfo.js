1c9fc64114c56b77108fc21fdf092ae9be5f9913
Dheenadhayalan <AmigoDheena@users.noreply.github.com>
2019-07-01 13:29:56 +0530

    Performance, Resource, Page info upgrade
    performance.js
    Resource info
    Page info

--- pageinfo.js
+++ pageinfo.js
@@ -1,41 +1,122 @@
-let hostnamee = document.location.hostname;
-let hostt = document.location.host;
-let hreff = document.location.href;
-let portt =  document.location.port;
-let protocoll = document.location.protocol;
+// performance.js
+  // https://github.com/bgrins/devtools-snippets
+  // Print out window.performance information.
+  // https://developer.mozilla.org/en-US/docs/Navigation_timing
 
-let firstPaint = chrome.loadTimes().firstPaintTime - chrome.loadTimes().startLoadTime;
-console.log(firstPaint);
+  (function () {
 
-// JS
-    let scriptt = document.querySelectorAll('script');
-    let scriptCount = [];
-    for (let i = 0; i < scriptt.length; i++) {
-        scriptCount.push(scriptt.value);
+    var t = window.performance.timing;
+    var lt = window.chrome && window.chrome.loadTimes && window.chrome.loadTimes();
+    var timings = [];
+
+    timings.push({
+      label: "Time Until Page Loaded",
+      time: t.loadEventEnd - t.navigationStart + "ms"
+    });
+    timings.push({
+      label: "Time Until DOMContentLoaded",
+      time: t.domContentLoadedEventEnd - t.navigationStart + "ms"
+    });
+    timings.push({
+      label: "Total Response Time",
+      time: t.responseEnd - t.requestStart + "ms"
+    });
+    timings.push({
+      label: "Connection",
+      time: t.connectEnd - t.connectStart + "ms"
+    });
+    timings.push({
+      label: "Response",
+      time: t.responseEnd - t.responseStart + "ms"
+    });
+    timings.push({
+      label: "Domain Lookup",
+      time: t.domainLookupEnd - t.domainLookupStart + "ms"
+    });
+    timings.push({
+      label: "Load Event",
+      time: t.loadEventEnd - t.loadEventStart + "ms"
+    });
+    timings.push({
+      label: "Unload Event",
+      time: t.unloadEventEnd - t.unloadEventStart + "ms"
+    });
+    timings.push({
+      label: "DOMContentLoaded Event",
+      time: t.domContentLoadedEventEnd - t.domContentLoadedEventStart + "ms"
+    });
+    if(lt) {
+      if(lt.wasNpnNegotiated) {
+        timings.push({
+          label: "NPN negotiation protocol",
+          time: lt.npnNegotiatedProtocol
+        });
+      }
+      timings.push({
+        label: "Connection Info",
+        time: lt.connectionInfo
+      });
+      timings.push({
+        label: "First paint after Document load",
+        time: Math.ceil(lt.firstPaintTime - lt.finishDocumentLoadTime) + "ms"
+      });
     }
-    // console.log(scriptCount.length);
-// JS
+    console.table(timings, ["label", "time"]);
+  })();
+// performance.js
 
-// CSS
-    let stylee = document.querySelectorAll('style');
-    let stylecount = [];
-    for (let s = 0; s < stylee.length; s++) {
-        stylecount.push(style.value);
+/* Resource info */
+  // script
+  var scriptsrc = document.querySelectorAll('script');
+  var arrscript = [];
+  for(var i=0; i<scriptsrc.length; i++){
+    if(scriptsrc[i].src != ''){
+        arrscript.push(scriptsrc[i].src);
     }
-    // console.log(stylecount.length);
-// CSS
+  }
+  console.table(arrscript);
+  // script
 
-// script
+  // image
+  var imagesrc = document.querySelectorAll('img');
+  var imgsrc = [];
+  for(i = 0; i<imagesrc.length; i++){
+    imgsrc.push(imagesrc[i].src);
+  }
+  console.table(imgsrc);
+  // image
 
-function webinfo() {
-    this.Href = hreff;
-    this.Host = hostt;
-    this.HostName = hostnamee;
-    this.Port = portt;
-    this.Protocol = protocoll;
-    this.FirstPaint = firstPaint;
-    this.JS = scriptCount.length;
-    this.CSS = stylecount.length;
-}
-let WebInfo = new webinfo();
-console.table(WebInfo);
+  // style
+  var teststyle = document.styleSheets;
+  var stylesrc = [];
+  for(i = 0; i<teststyle.length-1; i++){
+    stylesrc.push(teststyle[i].href);
+  }
+  console.table(stylesrc);
+  // style
+/* Resource info */
+
+/* Page info */
+  let hostnamee = document.location.hostname;
+  let hostt = document.location.host;
+  let hreff = document.location.href;
+  let portt =  document.location.port;
+  let protocoll = document.location.protocol;
+
+  let firstPaint = window.chrome.loadTimes().firstPaintTime - window.chrome.loadTimes().startLoadTime;
+  console.log(firstPaint);
+
+  function webinfo() {
+      this.Href = hreff;
+      this.Host = hostt;
+      this.HostName = hostnamee;
+      this.Port = portt;
+      this.Protocol = protocoll;
+      this.FirstPaint = firstPaint;
+      this.JS = arrscript.length;
+      this.CSS = stylesrc.length;
+      this.Image = imgsrc.length;
+  }
+  let WebInfo = new webinfo();
+  console.table(WebInfo);
+/* Page info */
